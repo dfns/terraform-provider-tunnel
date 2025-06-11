3dcc3eb283c8871a568c18e79715c29792d95a20
Clark Winters <github@clarkwinters.com>
2025-06-11 06:02:03 -0500

    conditionally update SSMRoleARN in ephemeral resource too

--- internal/provider/ephemeral_ssm.go
+++ internal/provider/ephemeral_ssm.go
@@ -125,7 +125,11 @@ func (d *SSMEphemeral) Open(ctx context.Context, req ephemeral.OpenRequest, resp
 
 	tunnelCfg.SSMRegion = awsCfg.Region
 	tunnelCfg.SSMProfile = ssm.GetSDKConfigProfile(awsCfg)
-	tunnelCfg.SSMRoleARN = ssm.GetSDKConfigRole(awsCfg)
+
+	// Only update SSMRoleARN if it wasn't explicitly provided
+	if tunnelCfg.SSMRoleARN == "" {
+		tunnelCfg.SSMRoleARN = ssm.GetSDKConfigRole(awsCfg)
+	}
 
 	data.SSMRegion = types.StringValue(tunnelCfg.SSMRegion)
 	data.SSMProfile = types.StringValue(tunnelCfg.SSMProfile)
