d26c337aaa7650c050a9b5edaf6201c93a984690
arnaud-dfns <arnaud@dfns.co>
2025-09-18 12:44:30 +0200

    ci(gha): add renovate and semantic release workflows

---
+++ .github/renovate-config.js
@@ -0,0 +1,21 @@
+/**
+ * @type {import('renovate/dist/config/types').AllConfig}
+ */
+module.exports = {
+  autodiscover: false,
+  branchPrefix: "renovate/",
+  dryRun: process.env.RENOVATE_REPOSITORIES ? null : "full",
+  enabledManagers: ["gomod", "github-actions"],
+  gitAuthor: "dfns-github-bot <infra@dfns.co>",
+  onboarding: false,
+  platform: "github",
+  prConcurrentLimit: 0,
+  prHourlyLimit: 0,
+  requireConfig: "optional",
+  lockFileMaintenance: {
+    enabled: false,
+    schedule: null,
+  },
+  packageRules: [],
+  customManagers: [],
+};
